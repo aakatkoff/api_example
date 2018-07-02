debf86fe18a7dd8069671ee667766bcf7e6790af
Носов Михаил <misha.n@ad1team.ru>
2018-07-02 16:39:22 +0300

    Revert

--- order.php
+++ order.php
@@ -1,6 +1,6 @@
 <?php
 
-require_once '../vendor/autoload.php';
+require_once 'SxGeo.php';
 
 if (isset($_SERVER['HTTP_X_REAL_IP'])) {
     $called_ip = $_SERVER['HTTP_X_REAL_IP'];
@@ -10,10 +10,9 @@ if (isset($_SERVER['HTTP_X_REAL_IP'])) {
     $called_ip = $_SERVER['REMOTE_ADDR'];
 }
 
+$SxGeo = new SxGeo(__DIR__ . '/SxGeo.dat');
+$countryKey = $SxGeo->get($called_ip);
 
-$phoneNumberUtil = \libphonenumber\PhoneNumberUtil::getInstance();
-$phoneNumber = $phoneNumberUtil->parse($_POST['phone'], 'ID');
-$countryKey = $phoneNumberUtil->getRegionCodeForCountryCode($phoneNumber->getCountryCode());
 
 $infocdnData = [
     'orders' => [
@@ -21,14 +20,14 @@ $infocdnData = [
             'country'           =>  $countryKey, // страна доставки
             'fio'               =>  $_POST['name'], // Имя
             'phone'             =>  $_POST['phone'], // Телефон
-            'user_ip'           =>  $called_ip, // ip сервера в данном случае
-            'user_agent'        =>  'FacebookWebhook', // UserAgent пользователя
+            'user_ip'           =>  $called_ip, // ip пользователя
+            'user_agent'        =>  $_SERVER['HTTP_USER_AGENT'], // UserAgent пользователя
             'order_time'        =>  time(), // timestamp времени заказа
         ]
     ],
     'system'    =>  [
         'network'   => 'ad1', // название сети
-        'thread'    => $_POST['thread'], // id потока из ad1.ru, например bakm
+        'thread'    => '', // id потока из ad1.ru, например bakm
         'subid'     => '', // 5 субайди, например subid1:subid2:subid3:subid4:subid5 (не обязательно)
         'site_key'  => '' // ключ
     ]
@@ -43,4 +42,5 @@ curl_setopt($handle, CURLOPT_RETURNTRANSFER, true);
 $result = curl_exec($handle);
 curl_close($handle);
 
-echo $result;
+//var_dump($result);
+header('Location: success.htm');
